5ac3bbcbe2537b3c0707a0c8488678cbc979bc06
ldh7228 <ldh7228@gmail.com>
2023-03-15 10:06:21 +0900

    Add GlobalStyle

---
+++ src/style/styles.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle: any = createGlobalStyle`
+    body {
+      -webkit-user-select: none;
+      -moz-user-select: none; 
+      -ms-user-select: none;
+      user-select: none;
+      background-color: #121212;
+      overflow-x: hidden;
+    }
+    body::-webkit-scrollbar {
+      /* display: none;   */
+    }
+
+    * {
+      outline: none;
+      text-decoration: none;
+    }
+
+    :root {
+      --white: #f0f0f0;
+      --black: #121212;
+      --pink: #eb4970;
+    }
+`;
+export default GlobalStyle;
