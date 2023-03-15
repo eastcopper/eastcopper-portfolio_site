64ef9d66645796d9a14d4bccc2f762806a766b0e
ldh7228 <ldh7228@gmail.com>
2023-03-15 15:55:40 +0900

    Update Zipper

--- src/components/common/Zipper/index.tsx
+++ src/components/common/Zipper/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { zipperScroll } from "../../../lib/func/zipperAnimation";
 import * as S from "./style";
 
@@ -11,10 +11,11 @@ export default function Zipper(): JSX.Element {
   const [divTransfrom, setDivTransfrom] = useState<string>("scale(1.5, 0)");
   const [position, setPosition] = useState("fixed");
   const [top, setTop] = useState(0);
+  const [color, setColor] = useState("#121212");
 
-  onscroll = () => {
+  document.addEventListener("scroll", () => {
     const scroll: number = document.documentElement.scrollTop; // 스크롤 감지
-    
+
     if (scroll >= 2020) {
       setPosition("absolute");
       setTop(2020);
@@ -27,18 +28,24 @@ export default function Zipper(): JSX.Element {
     divs.forEach(() => {
       setDivTransfrom(`scale(1.5, ${zipperScroll(scroll)})`); // 스크롤 시 div 세로 크기 조정
     });
-  };
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setColor("#f0f0f0");
+    }, 500);
+  }, []);
 
   return (
-    <S.MainDiv position={position}>
+    <S.MainDiv position={position} backColor={color}>
       <S.Title
-        color="#121212"
+        backColor="#121212"
         zindex={2}
         size={160}
         position={position}
         top={"50%"}
       >
-        PORTFOLIO
+        <span>PORTFOLIO</span>
       </S.Title>
       {divs.map((item: number) => (
         <span
@@ -53,12 +60,14 @@ export default function Zipper(): JSX.Element {
         /> // height를 동적으로 변화시켜 애니메이션 만들기
       ))}
       <S.Title
-        color="#f0f0f0"
+        backColor={color}
         zindex={1}
         size={200}
         position={position}
         top={top === 0 ? "50%" : "2477px"}
-      >{`Lee\ndong hyeon`}</S.Title>
+      >
+        <span>{`Lee\ndong hyeon`}</span>
+      </S.Title>
     </S.MainDiv>
   );
 }
