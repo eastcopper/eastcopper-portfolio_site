0676ca5921c3bffa4f3944c7a6cf44fabad86302
ldh7228 <ldh7228@gmail.com>
2023-03-15 10:06:37 +0900

    Add Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Zipper from "./components/common/Zipper";
+import GlobalStyle from "./style/styles";
+import Works from "./components/common/Works";
+import Introduce from "./components/common/Introduce";
+import Project from "./components/common/Project";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Zipper />
+      <Works />
+      <Introduce />
+      <Project />
+    </>
   );
 }
 
