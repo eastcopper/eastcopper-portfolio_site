fbd2bd9ce94dcfeb67c3ef06ce451644ffc1824d
ldh7228 <ldh7228@gmail.com>
2023-03-15 10:05:21 +0900

    Design Zipper

---
+++ src/components/common/Zipper/style.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div<{ position?: string; top?: number }>`
+  position: ${(props) => props.position || "fixed"};
+  z-index: 2;
+  top: ${(props) => props.top || 0};
+  width: 100%;
+  height: 100%;
+  background-color: var(--white);
+  display: flex;
+`;
+
+export const Title = styled.p<{
+  position?: string;
+  top?: string;
+  color: string;
+  zindex: number;
+  size: number;
+}>`
+  font-size: ${(props) => props.size}px;
+  z-index: ${(props) => props.zindex};
+  position: ${(props) => props.position || "fixed"};
+  left: 50%;
+  top: ${(props) => props.top};
+  transform: translate(-50%, -50%);
+  text-align: center;
+  line-height: 1.3;
+  margin: 0;
+  font-family: "Bebas Neue", cursive;
+  color: ${(props) => props.color};
+  white-space: pre-wrap;
+`;
